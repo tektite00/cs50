600b82e1f012f602a5506dcb944b8d19b5ed3716
tektite00 <hungrypie00@gmail.com>
2019-06-29 01:15:36 +0000

    Completed :ok_hand: pset7/similarities/less

--- pset7/similarities/less/helpers.py
+++ pset7/similarities/less/helpers.py
@@ -1,20 +1,27 @@
+from nltk.tokenize import sent_tokenize
+
+
 def lines(a, b):
     """Return lines in both a and b"""
-    a = a.split("\n")
-    b = b.split("\n")
-
-    return []
+    lines_a = set(a.split("\n"))
+    lines_b = set(b.split("\n"))
+    return list(lines_a.intersection(lines_b))
 
 
 def sentences(a, b):
     """Return sentences in both a and b"""
+    sents_a = set(sent_tokenize(a))
+    sents_b = set(sent_tokenize(b))
+    return list(sents_a.intersection(sents_b))
 
-    # TODO
-    return []
+
+def substr_tokenize(string, n):
+    """Tokenizes string into substrings of n size/length"""
+    return [string[i:n+i] for i in range(len(string) - n + 1)]
 
 
 def substrings(a, b, n):
     """Return substrings of length n in both a and b"""
-
-    # TODO
-    return []
+    substrings_a = set(substr_tokenize(a, n))
+    substrings_b = set(substr_tokenize(a, n))
+    return list(substrings_a.intersection(substrings_b))
