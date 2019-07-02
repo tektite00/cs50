7756a58d3fdf1ff2036b7c0572575ec6fa5afdb9
tektite00 <hungrypie00@gmail.com>
2019-07-01 21:53:08 -0500

    pset8 started

--- pset8/finance/application.py
+++ pset8/finance/application.py
@@ -1,6 +1,5 @@
 import os
 
-from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
